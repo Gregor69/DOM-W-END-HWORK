7bfc92697c464d6974d8a82877ca59370de22629
Gregor Simpson <catkelly@Catrionas-MacBook-Pro.local>
2021-02-14 11:05:21 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log('JavaScript has loaded');
+    const newFormEntry = document.querySelector('#new-form-entry');
+    newFormEntry.addEventListener('submit', handleNewFormEntrySubmit);
+
+});
+
+const createChampionsItem = function (form) {
+    const listOfChampionsItem = document.createElement('li');
+    listOfChampionsItem.classList.add('list-of-champions-item');
+
+    const rider = document.createElement('h2');
+    rider.textContent = form.rider.value;
+    listOfChampionsItem.appendChild(rider);
+
+    const team = document.createElement('h3');
+    team.textContent = form.team.value;
+    listOfChampionsItem.appendChild(team);
+
+    const year = document.createElement('h3');
+    year.textContent = form.year.value;
+    listOfChampionsItem.appendChild(year);
+
+    const points = document.createElement('h4');
+    points.textContent = form.points.value;
+    listOfChampionsItem.appendChild(points);
+
+    const victories = document.createElement('h4');
+    victories.textContent = form.victories.value;
+    listOfChampionsItem.appendChild(victories)
+
+    const DNFs = document.createElement('p');
+    DNFs.textContent = form.DNFs.value;
+    listOfChampionsItem.appendChild(DNFs);
+
+
+
+    return listOfChampionsItem;
+
+
+}
+
+const handleNewFormEntrySubmit = function (event) {
+    event.preventDefault()
+
+    const listOfChampionsItem = createChampionsItem(event.target);
+    const listOfChampions = document.querySelector('#list-of-champions');
+    listOfChampions.appendChild(listOfChampionsItem);
+
+    event.target.reset();
+}
+
+
